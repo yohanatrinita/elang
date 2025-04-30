589b61f8cbe038323033ac6db7efc14569e11833
Yohana Trinita <youwthree@gmail.com>
2025-04-30 14:25:32 +0700

    update lagi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ELANG – E-Arsip DLH</title>
+    <title>ELANG – Elektronik Arsip Lingkungan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap" rel="stylesheet">
 
